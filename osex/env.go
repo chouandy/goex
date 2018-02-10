1ff43b88ebe3547c4bda709b011881ecabfb453f
ChouAndy <chouandy@ecoworkinc.com>
2018-02-10 14:08:07 +0800

    Add osex env.go
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

---
+++ osex/env.go
@@ -0,0 +1,12 @@
+package osex
+
+import "os"
+
+// Getenv get env with fallback
+func Getenv(key string, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
