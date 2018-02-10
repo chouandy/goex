94d90d2e0f1b1c753755bfded4222af9cc9e8854
ChouAndy <chouandy@ecoworkinc.com>
2018-02-11 03:13:54 +0800

    Add jsonex/array.go - UnmarshalArrayJSON
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- jsonex/array.go
+++ jsonex/array.go
@@ -2,7 +2,34 @@ package jsonex
 
 import "github.com/mailru/easyjson/jlexer"
 
-// UnmarshalArrayJSONBytes unmarshal array JSON
+// UnmarshalArrayJSON unmarshal array JSON to []string
+func UnmarshalArrayJSON(data []byte, out *[]string) error {
+	r := jlexer.Lexer{Data: data}
+	unmarshalArrayJSON(&r, out)
+	return r.Error()
+}
+
+func unmarshalArrayJSON(in *jlexer.Lexer, out *[]string) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('[')
+	for !in.IsDelim(']') {
+		item := in.UnsafeBytes()
+		*out = append(*out, string(item))
+		in.WantComma()
+	}
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+
+// UnmarshalArrayJSONBytes unmarshal array JSON to [][]byte
 func UnmarshalArrayJSONBytes(data []byte, out *[][]byte) error {
 	r := jlexer.Lexer{Data: data}
 	unmarshalArrayJSONBytes(&r, out)
