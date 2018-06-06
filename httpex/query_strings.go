71069c974e646e7292f3cb66fc1a634a321673fa
ChouAndy <chouandy@ecoworkinc.com>
2018-06-06 15:20:49 +0800

    httpex/query_strings.go add AppendQueryStrings
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- httpex/query_strings.go
+++ httpex/query_strings.go
@@ -1,6 +1,9 @@
 package httpex
 
-import "net/url"
+import (
+	"net/url"
+	"strings"
+)
 
 // MergeQueryStrings merge query strings
 func MergeQueryStrings(origin *string, qs map[string]string) error {
@@ -20,4 +23,19 @@ func MergeQueryStrings(origin *string, qs map[string]string) error {
 	*origin = u.String()
 
 	return nil
+}
+
+// AppendQueryStrings apend query strings
+func AppendQueryStrings(origin *string, qs map[string]string) {
+	// Add query strings
+	params := url.Values{}
+	for k, v := range qs {
+		params.Add(k, v)
+	}
+	// Set raw query
+	if strings.Contains(*origin, "?") {
+		*origin = *origin + "&" + params.Encode()
+	} else {
+		*origin = *origin + "?" + params.Encode()
+	}
 }
--- httpex/query_strings_test.go
+++ httpex/query_strings_test.go
@@ -42,3 +42,52 @@ func TestMergeQueryStrings(t *testing.T) {
 		})
 	}
 }
+
+func TestAppendQueryStrings(t *testing.T) {
+	// Set test cases
+	testCases := []struct {
+		url      string
+		qs       map[string]string
+		expected string
+	}{
+		{
+			url: "https://www.google.com",
+			qs: map[string]string{
+				"a": "b",
+				"c": "d",
+			},
+			expected: `https://www.google.com?a=b&c=d`,
+		},
+		{
+			url: "https://www.google.com?a=b&c=d",
+			qs: map[string]string{
+				"user name": "andy chou",
+				"c":         "f",
+			},
+			expected: `https://www.google.com?a=b&c=d&c=f&user+name=andy+chou`,
+		},
+		{
+			url: "https://192.168.212.1/gui/#/main/login/sso?checkForGrantCode=1",
+			qs: map[string]string{
+				"code":  "e0cb7d55bad918599408d673804b6c28a4b0c9a5bf863ac2a3d707e0e7734151",
+				"token": "token",
+			},
+			expected: `https://192.168.212.1/gui/#/main/login/sso?checkForGrantCode=1&code=e0cb7d55bad918599408d673804b6c28a4b0c9a5bf863ac2a3d707e0e7734151&token=token`,
+		},
+		{
+			url: "https://192.168.212.1/gui/",
+			qs: map[string]string{
+				"code":  "e0cb7d55bad918599408d673804b6c28a4b0c9a5bf863ac2a3d707e0e7734151",
+				"token": "token",
+			},
+			expected: `https://192.168.212.1/gui/?code=e0cb7d55bad918599408d673804b6c28a4b0c9a5bf863ac2a3d707e0e7734151&token=token`,
+		},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("TestCase[%d]", i+1), func(t *testing.T) {
+			AppendQueryStrings(&testCase.url, testCase.qs)
+			assert.Equal(t, testCase.expected, testCase.url)
+		})
+	}
+}
