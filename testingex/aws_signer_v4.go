e83cb62a0d0b535e5b26c8d4df022661ebf29494
ChouAndy <chouandy@ecoworkinc.com>
2018-06-10 02:47:15 +0800

    testingex/aws_signer_v4.go add static credentials provider
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- testingex/aws_signer_v4.go
+++ testingex/aws_signer_v4.go
@@ -5,23 +5,38 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 )
 
 // AwsSignerV4 httpex testing aws signer v4
 type AwsSignerV4 struct {
-	Region      string
-	ServiceName string
+	Region          string
+	ServiceName     string
+	AccessKeyID     string
+	SecretAccessKey string
+	SessionToken    string
 }
 
 // Sign sign aws signature v4
 func (a *AwsSignerV4) Sign(r *http.Request, body io.ReadSeeker) (http.Header, error) {
-	cfg, err := external.LoadDefaultAWSConfig()
-	if err != nil {
-		panic(err)
+	var provider aws.CredentialsProvider
+	if len(a.AccessKeyID) > 0 && len(a.SecretAccessKey) > 0 {
+		// New credentials provider with access_key_id, secret_access_key
+		provider = aws.NewStaticCredentialsProvider(
+			a.AccessKeyID, a.SecretAccessKey, a.SessionToken,
+		)
+	} else {
+		// Load credentials provider form env
+		cfg, err := external.LoadDefaultAWSConfig()
+		if err != nil {
+			panic(err)
+		}
+		provider = cfg.Credentials
 	}
-	signer := v4.NewSigner(cfg.Credentials)
+	// New signer
+	signer := v4.NewSigner(provider)
 
 	return signer.Sign(r, body, a.ServiceName, a.Region, time.Now())
 }
