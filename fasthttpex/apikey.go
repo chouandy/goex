496abc86d01ca9a5826eff8e140fee05b13ab0ea
ChouAndy <chouandy@ecoworkinc.com>
2018-10-02 15:12:26 +0800

    Update fasthttpex/apikey.go with aws sdk v2
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- fasthttpex/apikey.go
+++ fasthttpex/apikey.go
@@ -5,11 +5,11 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	yaml "gopkg.in/yaml.v2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/s3manager"
+	"github.com/chouandy/goex/awsex"
+	"gopkg.in/yaml.v2"
 )
 
 // APIKey api key struct
@@ -23,43 +23,39 @@ type APIKey struct {
 
 // Upload upload apikey whitelist to s3
 func (c *APIKey) Upload(file *os.File) error {
-	// Initialize S3 uploader
-	awsConfig := &aws.Config{Region: aws.String(c.S3Region)}
-	uploader := s3manager.NewUploader(session.New(awsConfig))
-	// Init input variable
+	// New input
 	input := &s3manager.UploadInput{
 		Bucket: aws.String(c.S3Bucket),
 		Key:    aws.String(fmt.Sprintf("%s/%s", c.S3Prefix, c.S3Key)),
 		Body:   file,
 	}
 	// Upload to s3
-	_, err := uploader.Upload(input)
+	_, err := awsex.S3Uploader.Upload(input)
 
 	return err
 }
 
 // Load load apikey whitelist from s3
 func (c *APIKey) Load() error {
-	// Initialize S3 svc
-	awsConfig := &aws.Config{Region: aws.String(c.S3Region)}
-	svc := s3.New(session.New(awsConfig))
-	// 初始化 input 參數
+	// New input
 	input := &s3.GetObjectInput{
 		Bucket: aws.String(c.S3Bucket),
 		Key:    aws.String(fmt.Sprintf("%s/%s", c.S3Prefix, c.S3Key)),
 	}
-	// 取得 bucket 中的 object
-	result, err := svc.GetObject(input)
+	// New request
+	req := awsex.S3Client.GetObjectRequest(input)
+	// Send request
+	result, err := req.Send()
 	if err != nil {
 		return err
 	}
-	content, err := ioutil.ReadAll(result.Body)
+	body, err := ioutil.ReadAll(result.Body)
 	if err != nil {
 		return err
 	}
-	// Parse content
+	// Parse body
 	var whitelist []map[string]string
-	if err := yaml.Unmarshal(content, &whitelist); err != nil {
+	if err := yaml.Unmarshal(body, &whitelist); err != nil {
 		return err
 	}
 	// Set whitelist
