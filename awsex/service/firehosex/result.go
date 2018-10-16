d641ee67af5b7706108f906ecf228a12ceedca17
ChouAndy <chouandy@ecoworkinc.com>
2018-10-16 13:37:36 +0800

    Update firehosex result attempted, successful, failed to int64
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- awsex/service/firehosex/result.go
+++ awsex/service/firehosex/result.go
@@ -2,7 +2,7 @@ package firehosex
 
 // Result firehose result struct
 type Result struct {
-	Attempted  int `json:"attempted"`
-	Successful int `json:"successful"`
-	Failed     int `json:"failed"`
+	Attempted  int64 `json:"attempted"`
+	Successful int64 `json:"successful"`
+	Failed     int64 `json:"failed"`
 }
