730cad6db008c011341e0fad3e0dfafaadd9e9e2
ChouAndy <chouandy@ecoworkinc.com>
2018-10-16 13:41:08 +0800

    Add firehosex result inline
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- awsex/service/firehosex/result.go
+++ awsex/service/firehosex/result.go
@@ -1,8 +1,17 @@
 package firehosex
 
+import "fmt"
+
 // Result firehose result struct
 type Result struct {
 	Attempted  int64 `json:"attempted"`
 	Successful int64 `json:"successful"`
 	Failed     int64 `json:"failed"`
+}
+
+// Inline inline
+func (r *Result) Inline() string {
+	return fmt.Sprintf("attempted: %d, successful: %d, failed: %d",
+		r.Attempted, r.Successful, r.Failed,
+	)
 }
