62517794feee5c3faca2fd8f20b88f90e529f640
ChouAndy <chouandy@ecoworkinc.com>
2018-10-20 23:59:34 +0800

    ginex logger add not logged path
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- ginex/logger.go
+++ ginex/logger.go
@@ -9,6 +9,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var notLoggedPaths map[string]struct{}
+
 // logger logger struct
 type logger struct {
 	Timestamp string `json:"timestamp"`
@@ -25,6 +27,19 @@ func Logger() gin.HandlerFunc {
 	return LoggerWithWriter(gin.DefaultWriter)
 }
 
+// LoggerWithNotLogged logger with not logged
+func LoggerWithNotLogged(paths ...string) gin.HandlerFunc {
+	// Set not logged
+	if length := len(paths); length > 0 {
+		notLoggedPaths = make(map[string]struct{}, length)
+		for _, path := range paths {
+			notLoggedPaths[path] = struct{}{}
+		}
+	}
+
+	return LoggerWithWriter(gin.DefaultWriter)
+}
+
 // LoggerWithWriter logger with writer
 func LoggerWithWriter(out io.Writer) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -33,6 +48,11 @@ func LoggerWithWriter(out io.Writer) gin.HandlerFunc {
 
 		// Process request
 		c.Next()
+
+		// Not logged
+		if _, ok := notLoggedPaths[c.Request.URL.Path]; ok {
+			return
+		}
 
 		// New logger
 		data, err := jsonex.Marshal(&logger{
