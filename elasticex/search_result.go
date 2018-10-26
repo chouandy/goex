b16af68521086b9559a44ea6df6c2d18a136da02
ChouAndy <chouandy@ecoworkinc.com>
2018-10-26 18:11:21 +0800

    Update elasticex/search_result.go
    Signed-off-by: ChouAndy <chouandy@ecoworkinc.com>

--- elasticex/search_result.go
+++ elasticex/search_result.go
@@ -10,26 +10,25 @@ import (
 // ParseSearchResult parse search result
 func (c *SearchService) ParseSearchResult(result json.RawMessage) (json.RawMessage, error) {
 	// Decode search result
-	sr := new(elastic.SearchResult)
-	decoder := &elastic.DefaultDecoder{}
-	if err := decoder.Decode(result, sr); err != nil {
+	var sr elastic.SearchResult
+	if err := jsonex.Unmarshal(result, &sr); err != nil {
 		return nil, err
 	}
 
 	// Get search terms buckets
 	if c.TermsAggregation != nil && len(c.TermsAggregation.Fields) == 1 {
-		return c.GetSearchTermBuckets(sr)
+		return c.GetSearchTermBuckets(&sr)
 	}
 	if c.TermsAggregation != nil && len(c.TermsAggregation.Fields) == 2 {
-		return c.GetSearchTermsBuckets(sr)
+		return c.GetSearchTermsBuckets(&sr)
 	}
 	// Get search date histogram buckets
 	if c.DateHistogramAggregation != nil {
-		return c.GetSearchDateHistogramBuckets(sr)
+		return c.GetSearchDateHistogramBuckets(&sr)
 	}
 	// Get search hits
 	if c.Size > 0 {
-		return c.GetSearchHits(sr)
+		return c.GetSearchHits(&sr)
 	}
 
 	return nil, errors.New("Unsupported search result")
